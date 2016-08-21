566c49e5fffeca42ba337d5048f3d0e29c4442d1
janghongjin <redust1230@gmail.com>
2016-08-21 17:45:26 +0000

    haha

--- js/script.js
+++ js/script.js
@@ -268,15 +268,15 @@ $(document).ready(function () {
         -------------------------------  */
 
     $('#google-map').gMap({
-        latitude: 31.562130,
-        longitude: 74.319460,
-        maptype: 'TERRAIN',
+        latitude: 37.584063,
+        longitude: 127.058844,
+        //maptype: 'TERRAIN',
         scrollwheel: false,
-        zoom: 14,
+        zoom: 16,
         markers: [
             {
-                latitude: 31.562130,
-                longitude: 74.319460,
+                latitude: 37.584063,
+                longitude: 127.058844,
                 html: "I am Here!",
                 icon: {
                     image: "images/icon/map_marker.png",
